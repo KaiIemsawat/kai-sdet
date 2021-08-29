bbff3c7140c4eebefcb7b467c1c71c8ff9c67432
KaiIemsawat <kaiiemsawat@gmail.com>
2021-08-29 10:34:19 -0400

    2nd commit with first edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Kai I!!");    }
+        System.out.println("Kai I!!");
+        System.out.println("first edit!!!");
+    }
 }
